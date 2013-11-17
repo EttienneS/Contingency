2013db6805ae57fa34c3cccf11678480521b60bd
EttienneS <raidenets@gmail.com>
2013-11-17 20:54:32 +0200

    Initial Testing Project

---
+++ Contingency/Contingency/Game1.cs
@@ -0,0 +1,102 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+using Contingency.Units;
+#endregion
+
+namespace Contingency
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        List<Unit> units = new List<Unit>();
+        MouseState mouseStateCurrent, mouseStatePrevious;
+
+        Random r = new Random();
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            for (int i = 0; i < 10; i++)
+            {
+                units.Add(new Unit(20, 20, r.Next(GraphicsDevice.Viewport.Width), r.Next(GraphicsDevice.Viewport.Height), Color.Blue));
+                units.Add(new Unit(20, 20, r.Next(GraphicsDevice.Viewport.Width), r.Next(GraphicsDevice.Viewport.Height), Color.Red));
+            }
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            mouseStateCurrent = Mouse.GetState();
+
+            if (mouseStateCurrent.LeftButton == ButtonState.Pressed && mouseStatePrevious.LeftButton == ButtonState.Released)
+            {
+                Rectangle mouseState = new Rectangle(mouseStateCurrent.X, mouseStateCurrent.Y, 1, 1);
+
+                foreach (Unit u in units)
+                {
+
+
+                    if (u.CollisionRectangle.Intersects(mouseState))
+                    {
+                        //u.Location = Vector2.Add(u.Location, new Vector2(30, 30));
+                        u.Color = Color.White;
+                    }
+
+                }
+
+            }
+
+            mouseStatePrevious = mouseStateCurrent;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+
+            foreach (Unit unit in units)
+            {
+                spriteBatch.Draw(unit.GetSprite(GraphicsDevice), unit.Location, Color.White);
+            }
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+
+    }
+
+}
---
+++ Contingency/Contingency/Units/Unit.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Contingency.Units
+{
+    class Unit : ISprite
+    {
+        public Unit(int width, int height, int x, int y, Color color)
+        {
+            Width = width;
+            Height = height;
+            Location = new Vector2(x, y);
+            Color = color;
+
+            _collisionRectangle = new Rectangle(x, y, width, height);
+        }
+
+        public Vector2 Location { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Color Color;
+        private Rectangle _collisionRectangle;
+
+        public Rectangle CollisionRectangle
+        {
+            get
+            {
+                _collisionRectangle.X = (int)Location.X;
+                _collisionRectangle.Y = (int)Location.Y;
+
+                return _collisionRectangle;
+            }
+        }
+
+        public Texture2D GetSprite(GraphicsDevice graphicsDevice)
+        {
+            Texture2D texture = new Texture2D(graphicsDevice, Width, Height);
+
+            Color[] data = new Color[Width * Height];
+
+            for (int i = 0; i < data.Length; i++)
+                data[i] = Color;
+
+            texture.SetData(data);
+
+            return texture;
+        }
+    }
+}
